17db68ab5211a6b4ece2042c76156aca276a9663
ajraaa <azr.tech00@gmail.com>
2025-06-26 15:50:24 +0700

    fixed the front end didnt connected to the smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      host: "0.0.0.0",
+    },
+  },
 };
